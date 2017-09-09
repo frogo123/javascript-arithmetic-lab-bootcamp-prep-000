c97717689571039317f1d9da423d477b722dbaec
Eddie Fisher <eddief28@tiscali.co.uk>
2017-09-09 04:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-function add(a,b){
+function add(a, b){
   return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function divide(a, b){
+  return a / b;
+}
+
+function inc(n){
+  retrun n++;
+}
+
+function dec(n){
+  return n--;
+}
+
+function makeInt(n){
+  n = parseInt(n, 10)
+  return n;
 }
